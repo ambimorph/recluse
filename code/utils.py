7a36275eceaa392a5a1840b9211bfe70403422b3
Amber on abhyasa <amber@abhyasa>
2013-09-10 12:58:19 -0600

    Added utils.py with a function for opening compressed or regular unicode files.

---
+++ code/utils.py
@@ -0,0 +1,23 @@
+# utils.py
+
+import codecs, bz2, gzip
+
+def open_with_unicode(file_name, compression_type, mode):
+    assert compression_type in [None, 'gzip', 'bzip2']
+    assert mode in ['r', 'w']
+    if compression_type == None:
+        if mode == 'r':
+            return codecs.getreader('utf-8')(open(file_name, mode))
+        elif mode == 'w':
+            return codecs.getwriter('utf-8')(open(file_name, mode))
+    elif compression_type == 'gzip':
+        if mode == 'r':
+            return codecs.getreader('utf-8')(gzip.GzipFile(file_name, mode))
+        elif mode == 'w':
+            return codecs.getwriter('utf-8')(gzip.GzipFile(file_name, mode))
+    elif compression_type == 'bzip2':
+        if mode == 'r':
+            return codecs.getreader('utf-8')(bz2.BZ2File(file_name, mode))
+        elif mode == 'w':
+            return codecs.getwriter('utf-8')(bz2.BZ2File(file_name, mode))
+
